9c81f42d16064359cb23b756d60e6abafd11d6a7
Bixkog <wojciechoziebly@gmail.com>
2020-10-19 15:58:24 +0200

    Uncouple roles_wr retrieval and computing in API.

--- src/server.rs
+++ src/server.rs
@@ -1,27 +1,26 @@
 use rocket;
 use crate::request_processing;
 use crate::storage::result_storage;
+use rocket::response::content;
 
-#[get("/guild/<guild_id>")]
-async fn guild(guild_id: String) -> Option<String> {
+#[get("/guild/roles_wr/<guild_id>")]
+async fn roles_wr_req(guild_id: String) -> Option<content::Json<String>> {
     match result_storage::get_roles_wr_results(&guild_id) {
-        Ok(payload) => return Some(payload),
+        Ok(payload) => Some(content::Json(payload)),
         Err(e) => {
-            println!("Error during the reading of roles_wr result: {}. Recomputing roles_wr.", e);
+            println!("Error during the reading of roles_wr result: {}", e);
+            None
         }
-    };
+    }
+}
+
+#[post("/guild/process/<guild_id>")]
+async fn process_guild(guild_id: String) -> Option<()> {
     match request_processing::process_guild_request(&guild_id, /*update:*/ false).await {
-        Ok(()) => (),
+        Ok(()) => Some(()),
         Err(e) => {
             println!("Error during processing guild data: {}", e);
             return None
-        }
-    };
-    match result_storage::get_roles_wr_results(&guild_id) {
-        Ok(payload) => Some(payload),
-        Err(e) => {
-            println!("Error during the reading of roles_wr results: {}.", e);
-            None
         }
     }
 }
@@ -42,6 +41,6 @@ async fn health() -> &'static str {
 }
 
 pub async fn run() -> Result<(), Box<dyn std::error::Error>>  {
-    rocket::ignite().mount("/dotastats", routes![guild, start, stop, health]).launch().await?;
+    rocket::ignite().mount("/dotastats", routes![roles_wr_req, process_guild, start, stop, health]).launch().await?;
     Ok(())
 }
